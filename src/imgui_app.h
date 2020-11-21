f313e805d09f76c541e28bc36cb93b835c1b0821
Jean-Pierre Gygax <gygax@practicomp.ch>
2020-11-21 19:49:59 +0100

    Got basic ImGui loop working (Vulkan implementation)

---
+++ src/imgui_app.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+void imgui_init();
+
+bool imgui_continue();
+
+void imgui_new_frame();
+void imgui_frame_done();
+
+void imgui_cleanup();
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 #include <cstdio>
+#include <imgui.h>
 #include "./database.h"
 #include "./repo_reader.h"
+#include "./imgui_app.h"
 
 
-int main(int argc, char *argv[])
+int main(int, char **)
 {
     Conan::Database database;
-    Conan::Repository_reader repo_reader{database};
+    Conan::Repository_reader repo_reader{ database };
 
-    auto future = repo_reader.requeue_all();
+    auto initial_requeue = repo_reader.requeue_all();
 
-    std::cin.ignore();
+    imgui_init();
+
+    while (imgui_continue()) {
+        imgui_new_frame();
+
+        // ConanWindow();
+
+        imgui_frame_done();
+    }
+
+    return 0;
 }
